a725dd8b81fb13a55e4654a7f8352a760917949a
Gleb Shevchenko <glieb.shevchenko@gmail.com>
2023-03-23 22:06:43 +0200

    Initial commit

---
+++ src/test/java/application/notesservice/NotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package application.notesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
